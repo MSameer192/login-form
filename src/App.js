e283bea970f6575184ccccc9e42c85632f695b02
MSameer192 <sameer192.official@gmail.com>
2022-10-23 01:11:58 +0500

    React Login Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+
+  const [loginDetail, setLoginDetail] = useState()
+  const [loginPswd, setloginPswd] = useState()
+
+
+  function updateName(event) {
+    event.preventDefault();
+    alert(`User Email: ${loginDetail} and Password: ${loginPswd}`)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container mt-5" id="form" > 
+                <form className="mt-5 px-3" >
+                    <div className="my-5">
+                        <label htmlFor="userName" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="userName" onChange={(event)=>{setLoginDetail(event.target.value)}}/>
+                    </div>
+                    <div className="mb-3 my-5">
+                        <label htmlFor="password" className="form-label">password</label>
+                        <input type="password" className="form-control" id="password" onChange={(event)=>{setloginPswd(event.target.value)}} />
+                    </div>
+
+                    <div> 
+                        <button  type="submit" className="btn btn-primary my-5 px-5" onClick={updateName}>LOGIN</button>
+                    </div>
+                </form>
+            </div>
   );
+
 }
 
 export default App;
