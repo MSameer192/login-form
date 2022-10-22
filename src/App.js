ba038f748163055d6d52f09e4956f6af0e8fe27d
MSameer192 <sameer192.official@gmail.com>
2022-10-23 01:42:15 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -3,35 +3,52 @@ import React, { useState } from 'react';
 
 
 function App() {
-
-
-  const [loginDetail, setLoginDetail] = useState()
+  const [loginEmail, setLoginEmail] = useState()
   const [loginPswd, setloginPswd] = useState()
-
 
   function updateName(event) {
     event.preventDefault();
-    alert(`User Email: ${loginDetail} and Password: ${loginPswd}`)
+    alert(`User Email: ${loginEmail} and Password: ${loginPswd}`)
   }
-
-
   return (
-    <div className="container mt-5" id="form" > 
-                <form className="mt-5 px-3" >
-                    <div className="my-5">
-                        <label htmlFor="userName" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="userName" onChange={(event)=>{setLoginDetail(event.target.value)}}/>
-                    </div>
-                    <div className="mb-3 my-5">
-                        <label htmlFor="password" className="form-label">password</label>
-                        <input type="password" className="form-control" id="password" onChange={(event)=>{setloginPswd(event.target.value)}} />
-                    </div>
+  <>
+    <section class="vh-100" style={{backgroundColor:'#508bfc'}}>
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+            <div class="card shadow-2-strong" style={{bordeRadius: "1rem"}}>
+              <div class="card-body p-5 text-center">
 
-                    <div> 
-                        <button  type="submit" className="btn btn-primary my-5 px-5" onClick={updateName}>LOGIN</button>
-                    </div>
-                </form>
+                <h3 class="mb-5">Sign in</h3>
+
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="typeEmailX-2">Email</label>
+                  <input type="email" id="typeEmailX-2" class="form-control form-control-lg" onChange={(event)=>{setLoginEmail(event.target.value)}}/>
+                </div>
+
+                <div class="form-outline mb-4">
+                  <label class="form-label" for="typePasswordX-2">Password</label>
+                  <input type="password" id="typePasswordX-2" class="form-control form-control-lg" onChange={(event)=>{setloginPswd(event.target.value)}} />
+                </div>
+                <button class="btn btn-primary btn-lg btn-block px-5" type="submit" onClick={updateName}>Login</button>
+
+                <hr class="my-4"/>
+
+                <button class="btn btn-lg btn-block btn-primary" style={{backgroundColor:'#dd4b39'}}
+                  type="submit"><i class="fab fa-google me-2  px-2"></i> Sign in with google</button>
+                <button class="btn btn-lg btn-block btn-primary my-2" style={{backgroundColor:'#3b5998'}}
+                  type="submit"><i class="fab fa-facebook-f me-2"></i>Sign in with facebook</button>
+
+              </div>
             </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+  </>
+          
   );
 
 }
