0162a7830ac3932455f30e6525fe607b9ba800b5
MSameer192 <sameer192.official@gmail.com>
2022-11-03 13:06:24 +0500

    FilterForm Updated

--- src/components/FilterForm.jsx
+++ src/components/FilterForm.jsx
@@ -1,44 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function FilterForm() {
-    const [number, setNumber] = useState();
-    const [list, setList] = useState([]);
+  const [number, setNumber] = useState();
+  const [list, setList] = useState([]);
+
+  const filterArray = () => {
+    setList([...list, number]);
+    setNumber("");
+  };
+
+  return (
+    <>
+      <form
+        action=""
+        className="container mt-5 text-center"
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        Please put your number :
+        <input
+          className="mx-3 "
+          type="text"
+          name=""
+          id=""
+          value={number}
+          onChange={(event) => {
+            setNumber(event.target.value);
+          }}
+        />
+        <button className="btn btn-primary" onClick={filterArray}>
+          {" "}
+          Filter Array{" "}
+        </button>
+      </form>
+
+      <div className="container text-center mt-5">
+
+        Input all numbers of data mentioned below:
+
+        {
+            list.map(mainArray =>(
+                <li> {mainArray} </li>
+            ))
+
+        }  
+
+        <br /> <br /> <br />
+
+        Negative Numbers mentioned below:
+        {
+            list.filter(pNum => pNum < 0).map(positiveNum => (
+                <li>
+                    {positiveNum}
+                </li>
+            ))                          
+        }
+        <br /> <br /> <br />
 
 
+        Positive Numbers mentioned below:
+        {
+            list.filter(nNum => nNum > 0).map(negativeNum => (
+                <li>
+                    {negativeNum}
+                </li>
+            ))
+        }
 
-    const filterArray = () => {
-        setList([...list, number])
-        setNumber('')
-    }
+      </div>
 
-
-
-    return (
-        <>
-            <form action="" className='container mt-5' onSubmit={(event) => { event.preventDefault() }}>
-                Please put your number :
-                <input className='mx-3 ' type="text" name="" id="" value={number} onChange={(event) => { setNumber(event.target.value) }} />
-
-                <button className='btn btn-primary' onClick={filterArray}> Filter Array </button>
-            </form>
-
-            <div className="container">
-
-                {
-                   
-
-
-                        list.filter((item) => {item < 0 })
-
-                        return(
-                            <p>{item}</p>
-                        )
-                }
-
-
-
-            </div>
-        </>
-
-    );
+    </>
+  );
 }
